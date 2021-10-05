1a8be9f510e04f255fe918d2a044adc0c00d1826
Alena <AleEnachka@yandex.ru>
2021-10-05 21:23:44 +0500

    lesson 3.25

--- src/js/script.js
+++ src/js/script.js
@@ -134,19 +134,22 @@ $(document).ready(function(){
         });
 
 
-        //Smooth scroll and pageup
+        // Smooth scroll and pageup
+
         $(window).scroll(function() {
-            if ($(this).scrollTop()>1600) {
+            if ($(this).scrollTop() > 1600) {
                 $('.pageup').fadeIn();
             } else {
                 $('.pageup').fadeOut();
             }
         });
 
-        $("a[href^='#']").click(function() {
+        $("a[href=#up]").click(function(){
             const _href = $(this).attr("href");
             $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
             return false;
         });
+
+        new WOW().init();
 
   });
